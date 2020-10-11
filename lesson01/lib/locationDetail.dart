96d1b20b64403d18c0c48bb7402c4e4b1f992cfc
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-10-11 11:46:25 -0300

    styling components

--- lesson01/lib/locationDetail.dart
+++ lesson01/lib/locationDetail.dart
@@ -34,14 +34,22 @@ class LocationDetail extends StatelessWidget {
   }
 
   Widget _sectionTitle(String text) {
-    return Text(text);
+    return Container(
+        padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 10.0),
+        child: Text(text,
+            textAlign: TextAlign.left,
+            style: TextStyle(fontSize: 25.0, color: Colors.black)));
   }
 
   Widget _sectionText(String text) {
-    return Text(text);
+    return Container(
+        padding: EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 15.0),
+        child: Text(text));
   }
 
   Widget _bannerImage(String url, double height) {
-    return Container(child: Image.network(url, fit: BoxFit.fitWidth));
+    return Container(
+        constraints: BoxConstraints.tightFor(height: height),
+        child: Image.network(url, fit: BoxFit.fitWidth));
   }
 }
