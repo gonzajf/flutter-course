b913c1152e1f5ad2e5f037e328292049add9eb03
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-10-11 19:09:33 -0300

    adding styles file

--- lesson01/lib/locationDetail.dart
+++ lesson01/lib/locationDetail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lesson01/styles.dart';
 import 'models/location.dart';
 
 class LocationDetail extends StatelessWidget {
@@ -36,9 +37,8 @@ class LocationDetail extends StatelessWidget {
   Widget _sectionTitle(String text) {
     return Container(
         padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 10.0),
-        child: Text(text,
-            textAlign: TextAlign.left,
-            style: TextStyle(fontSize: 25.0, color: Colors.black)));
+        child:
+            Text(text, textAlign: TextAlign.left, style: Styles.headerLarge));
   }
 
   Widget _sectionText(String text) {
