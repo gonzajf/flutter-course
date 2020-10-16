d2b1f0c7d880aa1497a814a01c6adf5d96d228d4
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-10-15 21:16:16 -0300

    fixes on build gradle

--- todoApp/lib/main.dart
+++ todoApp/lib/main.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
 
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Welcome to flutter!',
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Welcome to flutter!'),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return const Scaffold(
+            body: Center(
+              child: Text('Error'),
             ),
-            body: const Center(
-              child: Text('Hello World!'),
-            )));
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Root();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return const Scaffold(
+          body: Center(
+            child: Text('Loading...'),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class Root extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
