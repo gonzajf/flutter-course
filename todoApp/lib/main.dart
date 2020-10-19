c9ab1313e512ef901ef9d307f2eebda85f05db38
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-10-18 22:41:42 -0300

    update main file

--- todoApp/lib/main.dart
+++ todoApp/lib/main.dart
@@ -1,55 +1,84 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-// Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
+import 'package:todoApp/services/auth.dart';
+import 'package:todoApp/screens/home.dart';
+import 'package:todoApp/screens/login.dart';
 
 void main() {
   runApp(App());
 }
 
 class App extends StatelessWidget {
-  // Create the initialization Future outside of `build`:
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: FutureBuilder(
+        // Initialize FlutterFire:
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return const Scaffold(
+              body: Center(
+                child: Text("Error"),
+              ),
+            );
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Root();
+          }
+
+          // Otherwise, show something whilst waiting for initialization to complete
           return const Scaffold(
             body: Center(
-              child: Text('Error'),
+              child: Text("Loading..."),
             ),
           );
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Root();
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return const Scaffold(
-          body: Center(
-            child: Text('Loading...'),
-          ),
-        );
-      },
+        },
+      ),
     );
   }
 }
 
 class Root extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _RootState();
+  _RootState createState() => _RootState();
 }
 
 class _RootState extends State<Root> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+      stream: Auth(_auth).user,
+      builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          if (snapshot.data?.uid == null) {
+            return Login(
+              auth: _auth,
+              firestore: _firestore,
+            );
+          } else {
+            return Home(
+              auth: _auth,
+              firestore: _firestore,
+            );
+          }
+        } else {
+          return const Scaffold(
+            body: Center(
+              child: Text("Loading..."),
+            ),
+          );
+        }
+      }, //Auth stream
+    );
   }
 }
