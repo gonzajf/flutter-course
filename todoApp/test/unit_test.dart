2de209471d41acbd55cb9cd03c973b533bcddf20
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-10-22 21:00:57 -0300

    configure and test firebase auth with mock object

---
+++ todoApp/test/unit_test.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:todoApp/services/auth.dart';
+
+class MockUser extends Mock implements User {}
+
+final MockUser _mockUser = MockUser();
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {
+  Stream<User> authStateChanges() {
+    return Stream.fromIterable([_mockUser]);
+  }
+}
+
+void main() {
+  final MockFirebaseAuth mockFirebaseAuth = MockFirebaseAuth();
+  final Auth auth = Auth(mockFirebaseAuth);
+  setUp(() {});
+  tearDown(() {});
+
+  test("emit occurs", () async {
+    expectLater(auth.user, emitsInOrder([_mockUser]));
+  });
+}
